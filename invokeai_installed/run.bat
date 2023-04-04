9342a03edfbbc608fd7dc8ce19c31c22b021cf4b
郵差 <twHenryYang@gmail.com>
2023-04-04 22:01:59 +0800

    update: WEB URL bind any address when launched

--- invokeai_installed/run.bat
+++ invokeai_installed/run.bat
@@ -1,3 +1,5 @@
 REM https://github.com/invoke-ai/InvokeAI#command-line-installation-for-users-familiar-with-terminals
-.venv\Scripts\activate && invokeai --web --no-nsfw_checker
+.venv\Scripts\activate && invokeai --web --host 0.0.0.0 --no-nsfw_checker
 REM https://invoke-ai.github.io/InvokeAI/features/NSFW/
+REM >> Started Invoke AI Web Server!
+REM >> Default host address now 127.0.0.1 (localhost). Use --host 0.0.0.0 to bind any address.
