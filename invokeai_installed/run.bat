c5ba5c48f7f2e0a505dc137a65d0bcd74b7597fd
郵差 <twHenryYang@gmail.com>
2023-04-04 12:35:40 +0800

    Create run.bat

---
+++ invokeai_installed/run.bat
@@ -0,0 +1,3 @@
+REM https://github.com/invoke-ai/InvokeAI#command-line-installation-for-users-familiar-with-terminals
+.venv\Scripts\activate && invokeai --web --no-nsfw_checker
+REM https://invoke-ai.github.io/InvokeAI/features/NSFW/
